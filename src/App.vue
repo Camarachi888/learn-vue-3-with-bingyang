f19a46daee380a1e3776ee4164491fa7d3309f15
Camarachi888 <amarachichiemela63@gmail.com>
2026-03-09 13:54:14 -0500

    Text interpolation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <template>
-  <div>Hello, Vue!</div>
+  <h1>message: {{ message }}</h1>
+  <hr />
+  <h1>count: {{ count }}</h1>
+  <hr />
+  <h1>number: {{ number }}</h1>
+  <hr />
+  <h1> doubleNum(50): {{ doubleNum(50) }}</h1>
+  <hr />
+  <h1> number *2 = {{ number * 2 }}</h1>
+  <h1> 
+    {{ number > 150 ? 'number is greater than 150' : 'number is less than 150' }}
+  </h1>
+
 </template>
 
-<script setup></script>
+<script setup>
+import { h } from 'vue';
+
+let message = 'Hello, Vue!';
+let count = 0;
+let number = 50;
+
+
+</script>
 
 <style lang="scss" scoped></style>
