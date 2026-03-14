78dc67bba35b3924090f4648f16abe3576dc9fa1
Camarachi888 <amarachichiemela63@gmail.com>
2026-03-14 12:06:57 -0500

    Event Handling v-on

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,46 @@
 <template>
-  <div>Hello, Vue!</div>
+  <h1>{{ message }}</h1>
+  <button v-on:click="replaceText('v-on is fun!')">Replace text</button>
+  <button @click="replaceText('v-on is fun!')">Replace text</button>
+  <hr>
+  <p>An input field where the user cn only enter numbers:</p>
+  <input type="text" @keydown="handleInput($event)"/>
+
+  <hr />
+  <div
+   style="width: 100px; height: 100px; background-color: aqua"
+   @contextmenu.prevent="cosole.log('Show custom context menu')">
+  </div>
+   
+    <div id="mouseover" @mouseover="fun1">
+    <textarea @mouseover="fun2($event)"> This is a text area.</textarea>
+    </div>
+   Hover over me!
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
 
-<style lang="scss" scoped></style>
+let message = ref('Hello, V-on!')
+function replaceText(msg) {
+  message.value = msg
+}
+
+function handleInput(event) {
+  console.log(event)
+  let keyCode = event.keyCode
+  if(keyCode < 48 || keyCode > 57) {
+    event.preventDefault()
+  }
+}
+</script>
+
+<style scoped>
+#mouseover {
+  margin: 100px;
+  text-align: right;
+  background-color: purple;
+  width: 300px;
+  height: 300px;
+}
+</style>
