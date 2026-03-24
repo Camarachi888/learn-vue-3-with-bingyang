92fa050bba25ea25102552324c193ecd481853ca
Camarachi888 <amarachichiemela63@gmail.com>
2026-03-24 08:59:19 -0500

    Using named slots

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>Hello, Vue!</div>
+  <BaseLayout>
+  <template #header> <!-- v-slot:header is the same as #header -->
+    <h1>Here might be a page title.</h1>
+  </template>
+  <template #main>
+    <p>A paragraph of the main content</p>
+    <p>And another one.</p>
+  </template>
+  <template #footer>
+    <p>Here's some contact info.</p>
+  </template>
+  </BaseLayout>
 </template>
 
-<script setup></script>
+<script setup>
+import BaseLayout from './BaseLayout.vue';
+
+</script>
 
 <style lang="scss" scoped></style>
