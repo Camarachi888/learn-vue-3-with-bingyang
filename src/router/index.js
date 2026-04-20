077f97632d1b9c7504cf922792edc2ad3159b31e
Camarachi888 <amarachichiemela63@gmail.com>
2026-04-20 05:14:34 -0500

    Named views.

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,10 @@ import { createRouter, createWebHistory } from "vue-router"
 import Home from "@/views/Home.vue"
 import BlogPosts from "@/views/BlogPosts.vue"
 import About from "@/views/About.vue"
+import BlogPost from "@/views/BlogPost.vue"
+import BlogPostsGreeting from '@/views/BlogPostsGreeting.vue'
+import NotFound from '@/views/NotFound.vue'
+import Ads from '@/views/Ads.vue'
 
 //create a router instance
 const router = createRouter({
@@ -18,11 +22,19 @@ const router = createRouter({
             redirect: { name: 'blogPostsGreeting' },
             children: [
                 { path: '', name: 'blogPostsGreeting', component: BlogPostsGreeting },
-                {path: '/blogPosts/:id(\\d+)', name: 'blogPost', component: BlogPost },
+                {path: '/blogPosts/:id(\\d+)', name: 'blogPost', components: {
+                    default: BlogPost,
+                    sidebar: Ads, }
+                } ,
             ],
         },
-        {path: '/about', name: 'about', component: About}
-    ]
+          { path: '/about', name: 'about', component: About },
+    {
+      path: '/:pathMatch(.*)*', // Match any path that hasn't been matched by a previous route
+      name: 'notFound',
+      component: NotFound,
+    },
+    ],
 })
 
 //export router instance
--- src/views/BlogPosts.vue
+++ src/views/BlogPosts.vue
@@ -1,12 +1,38 @@
 <template>
     <div>
-        <h1>Welcome to the blog posts page!</h1>
-        <p>
-            Here you can find all the blog posts that have been 
-            published by our authors. Click on any of them to read more.
-        </p>
+       <aside class="blog-post-list">
+            <BlogPostList></BlogPostList>
+        </aside>
+        <main class="blog-post-content-with-sidebar">
+            <!--default router view-->
+            <router-view class="blog-post-content"></router-view>
+            <!--named router view-->
+            <router-view name="sidebar"></router-view>
+        </main>
     </div>
 </template>
 
-<script setup> </script>
-<style lang="scss" scoped></style>
+<script setup> 
+import BlogPostList from '@/components/BlogPostList.vue'
+</script>
+
+<style lang="scss" scoped>
+.blog-posts-layout {
+    display: flex;      
+    width: 100%;
+    gap: 1rem;
+
+    .blog-post-list {
+        flex: 0 0 25%
+    }
+
+     .blog-post-content-with-sidebar {
+        display: flex;
+        flex: 0 0 75%;
+
+        .blog-post-content {
+            flex: 1; // Take up the remaining space
+        }
+    }
+}
+</style>
